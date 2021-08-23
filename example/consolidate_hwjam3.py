495f06ae2d6bc0fab3c5746e9e186fad6c5d1f24
Aiden Dax <strangebit@gmail.com>
2021-08-23 10:42:53 +0100

    Clean up hwjam3 example code and add warning

--- example/consolidate_hwjam3.py
+++ example/consolidate_hwjam3.py
@@ -10,29 +10,32 @@ from zipfile import ZipFile
 
 from consolidate_quake_gamedir import consolidate, PrintOut
 
-mod = 'hwjam3'
+# Not guaranteed to work with any arbitrary mod as they may be packaged differently.
+# For example, Halloween Jam 3 lacks a root folder in the zip. Some mods do not.
+# This script has no heuristic for this, so change mod_gamedir at your own risk.
+mod_gamedir = 'hwjam3'
 mod_name = "Halloween Jam 3"
-mod_zip = mod + ".zip"
+mod_zip = mod_gamedir + ".zip"
 mod_url = 'https://www.quaddicted.com/filebase/' + mod_zip
 
 def download_mod():
     if not os.path.isfile(mod_zip):
-        print(f'{mod_name} ({mod_zip}) not found.')
-        print(f'Downloading {mod_name} from: {mod_url}')
+        print(f'{mod_name} ({mod_zip}) not found')
+        print(f'Downloading {mod_name} from {mod_url}')
         print('  Please wait; this may take a while...')
         r = requests.get(mod_url)
         open(mod_zip, 'wb').write(r.content)
 
 def extract_mod():
-    print(f'Deleting any existing {mod} gamedir')
-    shutil.rmtree(mod, True)
+    print(f'Deleting any existing {mod_gamedir} gamedir')
+    shutil.rmtree(mod_gamedir, True)
 
-    print(f'Extracting {mod_name} ({mod_zip}) to {mod} gamedir')
+    print(f'Extracting {mod_name} ({mod_zip}) to {mod_gamedir} gamedir')
     with ZipFile(mod_zip, 'r') as zipObj:
-        zipObj.extractall(mod)
+        zipObj.extractall(mod_gamedir)
 
 def consolidate_mod():
-    consolidate(mod, PrintOut.VERBOSE)
+    consolidate(mod_gamedir, PrintOut.VERBOSE)
 
 if __name__ == '__main__':
     # Change current dir to example subdir
